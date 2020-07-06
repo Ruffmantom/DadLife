dbaaebbf83342ead73d127133ebd75e2db194556
ruffmantom <tomruffcodes@gmail.com>
2020-07-06 11:38:26 -0600

    got the sign up/ sign in  page finished and now working with react. need to start on card components for jokes and community page

--- src/Pages/Signup/index.js
+++ src/Pages/Signup/index.js
@@ -1,13 +1,49 @@
 import React from 'react';
-import "./style.css"
+import "./style.css";
+import logo from "../../assets/images/icons/blackIcons/logoBK.png"
 
 
 export default function Signup() {
-    
+
     return (
         <div>
-           hello world!
-            
+            <div className="container">
+                <div className="logoContainer">
+                    <img src={logo} alt="dadlife logo" id="logo" />
+                </div>
+                <div className="form-container">
+                    <h2 className="leadTitle">Welcome to DadLife</h2>
+                    <p className="desc">Sign In</p>
+                    <form className="sign-in" action="submit">
+                        <p className="label">Username</p>
+                        <input type="text" name="username" placeholder="Username" />
+                        <p className="label">Password</p>
+                        <input type="password" name="password" placeholder="Password" />
+                        <br />
+                        <button type="submit" className="main-btn">Log In</button>
+                    </form>
+                    <div id="slider" className="slider" ontouchstart="slideUp()">
+                        <div className="slide-icon">
+                        </div>
+                        <p id="slide-up-desc" className="desc">Swipe to sign up</p>
+                        <div id="sign-up-container">
+                            <h2 className="leadTitle sign-up-title">Sign Up</h2>
+                            <p className="desc">Join the DadLife Community</p>
+                            <form className="sign-up-form" action="submit">
+                                <p className="label">Username</p>
+                                <input type="text" name="username" placeholder="Choose a username" />
+                                <p className="label">Password</p>
+                                <input type="password" name="password" placeholder="Choose a password" />
+                                <p className="label">Confirm Password</p>
+                                <input type="password" name="password" placeholder="Re enter password" />
+                                <br />
+                                <button type="submit" className="main-btn">Log In</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
