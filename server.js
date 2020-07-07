d5aacde721cfddf440f699c7a4c0452f8028e579
ruffmantom <tomruffcodes@gmail.com>
2020-07-07 13:13:04 -0600

    added a controller file. dont know where to start

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const path = require("path");
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
