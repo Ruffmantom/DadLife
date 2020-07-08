4663d23d61b4044949cfb6495dd2c52a97319592
ruffmantom <tomruffcodes@gmail.com>
2020-07-08 14:06:12 -0600

    got the schema made

---
+++ models/posts.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    usernName: {type: String, required:true},
+    decsription: {type: String, required:true},
+    date: { type: Date, default: Date.now }
+})
+const Post = mongoose.model("Post", postSchema);
+module.exports = Post;
