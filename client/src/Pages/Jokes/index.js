88b52f129ab72366bd136a3ec613c0bfb8fe9e03
ruffmantom <tomruffcodes@gmail.com>
2020-07-09 13:00:23 -0600

    i need to go skate

--- client/src/Pages/Jokes/index.js
+++ client/src/Pages/Jokes/index.js
@@ -4,6 +4,7 @@ import Footer from "../../Components/Footer";
 // import components
 import JokeCard from "../../Components/JokeCard";
 import "./style.css";
+import { startSession } from 'mongoose';
 
 
 // ajax call to save saved joke to database
@@ -13,36 +14,33 @@ import "./style.css";
 export default function Jokes() {
     var jokesTitle = "Dad Jokes";
     let [jokeArr, setJokeArr] = useState([]);
-    // const jokeArr = [];
 
     useEffect(() => {
         jokeSearch()
         console.log("jokeArr inside of useEffect")
         console.log(jokeArr)
         console.log("----------------------------")
-    },[])
+    }, [])
 
     // joke api call
     function jokeSearch() {
         // for some reason the slack api link works best
         var queryURL = "https://icanhazdadjoke.com/slack";
-        for (var i = 0; i < 11; i++){
+        for (var i = 0; i < 10; i++) {
             fetch(
                 queryURL,
                 {
-                  method: "GET",
+                    method: "GET",
                 }
-              )
+            )
                 .then(res => res.json())
                 .then(response =>
-                //   this.setJokeArr()
-                // console.log(response.attachments[0].text);
-                setJokeArr(jokeArr.push(response.attachments[0].text))
+                    setJokeArr(jokeArr.push(response.attachments[0].text))
                 )
                 .catch(error => console.log(error));
         }
-        
-        
+
+
     };
 
     return (
@@ -51,11 +49,11 @@ export default function Jokes() {
                 pageTitle={jokesTitle.toUpperCase()}
             />
             {/* content here */}
-            
-                <JokeCard
-                    joke={jokeArr}
-                />
-            
+
+            <JokeCard
+                joke={jokeArr}
+            />
+
             <Footer />
         </div>
     )
