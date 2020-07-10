c8e51470fd26362754f198572eba4054f751f3ab
ruffmantom <tomruffcodes@gmail.com>
2020-07-10 17:28:10 -0600

    trying to get this api call working but cant figure out why it wont work

--- client/src/Pages/Jokes/index.js
+++ client/src/Pages/Jokes/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
 // import components
@@ -9,66 +9,55 @@ import { startSession } from 'mongoose';
 import comment from "../../assets/images/newicons/comment.png";
 import profile from "../../assets/images/newicons/profile.png";
 import plusFilled from "../../assets/images/newicons/plus-filled.png";
+// import function for API call
+import jokeSearch from "../../utils/jokeSearch";
+
+var jokesTitle = "Dad Jokes";
+// const [jokeArr, setJokeArr] = useState([]);
+// jokeSearch.jokeSearch(jokeArr, setJokeArr);
+// console.log(jokeArr)
 
 
+class Jokes extends React.Component {
 
-// ajax call to save saved joke to database
-// get save button to work
-
-
-export default function Jokes() {
-    var jokesTitle = "Dad Jokes";
-    const [jokeArr, setJokeArr] = useState([]);
-
-    useEffect(() => {
-        jokeSearch()
-
-        console.log("jokeArr inside of useEffect")
-        console.log(jokeArr)
-        console.log("----------------------------")
-
-    }, [])
-
-    // joke api call
-    function jokeSearch() {
-        // for some reason the slack api link works best
-        var queryURL = "https://icanhazdadjoke.com/slack";
-        for (var i = 0; i < 10; i++) {
-            fetch(
-                queryURL,
-                {
-                    method: "GET",
-                }
-            )
-                .then(res => res.json())
-                .then(response =>
-                    setJokeArr(jokeArr.push(response.attachments[0].text))
-                )
-                .catch(error => console.log(error));
-        }
-
-
+    state = {
+        jokes: []
     };
+    componentDidMount(){
+        for (var i = 0; i < 5; i++) (
+            jokeSearch.search()
+            
+        )
+        .then(res=>{
+            this.state.jokes.push(res)
+        }) 
+    }
 
-    return (
-        <div className="joke-body">
-            <Header
-                pageTitle={jokesTitle.toUpperCase()}
-            />
-            {/* content here */}
-            {jokeArr.length > 0 && (
-                jokeArr.map(res => (
-                    <JokeCard
-                        joke={res}
+    render() {
+        const j = this.state;
+        // jokeSearch.jokeSearch(this.state.jokes)
+        console.log(j)
+        return (
+            <div className="joke-body">
+                <Header
+                    pageTitle={jokesTitle.toUpperCase()}
+                />
+                {/* content here */}
+                {
+                 j.jokes.map(item=>(<JokeCard joke={item}/>))   
+                }
+                <Footer
+                    jokes={plusFilled}
+                    community={comment}
+                    profile={profile}
+                />
+            </div>
 
-                    />
-                )))
-            }
-            <Footer
-                jokes={plusFilled}
-                community={comment}
-                profile={profile}
-            />
-        </div>
-    )
+        )
+
+    }
+
 }
+
+
+export default Jokes;
