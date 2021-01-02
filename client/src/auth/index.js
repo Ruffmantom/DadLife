f4f7a0bd32abe3de79e2ffb33ca5a2c6e48c0e42
Thomas Ruff <thomasruff@thomass-air.lan>
2021-01-02 16:29:00 -0700

    the sign in and sign up is working

--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -1,22 +1,22 @@
 import { API } from '../config.js';
 
 export const signUp = (user) => {
-    console.log(user);
-    console.log(API);
-    // return fetch(`${API}/signup`, {
-    //     method: "POST",
-    //     headers: {
-    //         Accept: 'application/json',
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify(user)
-    // })
-    //     .then(res => {
-    //         return res.json()
-    //     })
-    //     .catch(err => {
-    //         console.log(err)
-    //     })
+    // console.log(user);
+    // console.log(API);
+    return fetch(`${API}/signup`, {
+        method: "POST",
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+        .then(res => {
+            return res.json()
+        })
+        .catch(err => {
+            console.log(err)
+        })
 }
 export const signIn = (user) => {
     return fetch(`${API}/signin`, {
