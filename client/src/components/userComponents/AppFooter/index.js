a5e28c4ce20355b914083956e8b648ac5892d89b
Thomas Ruff <thomasruff@thomass-air.lan>
2021-01-10 13:02:28 -0700

    the footer is good to go now. styles are all finished for it

--- client/src/components/userComponents/AppFooter/index.js
+++ client/src/components/userComponents/AppFooter/index.js
@@ -27,21 +27,21 @@ export default function AppFooter({ page }) {
   }, []);
   return (
     <div className="dl-footer">
-      <Link className="dl-f-l" to={`/auth/jokes/${user._id}`}>
+      <Link className={ jokesActive? `dlfl-l-active dlf-l dl-f-l`: `dl-f-l dlf-l`} to={`/auth/jokes/${user._id}`}>
         {jokesActive ? (
           <img className="dl-f-icon dl-f-active" src={jokesActiveIcon} alt="#" />
         ) : (
           <img className="dl-f-icon" src={jokesIcon} alt="#" />
         )}
       </Link>
-      <Link className="dl-f-l" to={`/auth/${user._id}`}>
+      <Link className={ profileActive? `dlfl-m-active dlf-m dl-f-l`: `dl-f-l dlf-m`} to={`/auth/${user._id}`}>
         {profileActive ? (
           <img className="dl-f-icon dl-f-active" src={profileActiveIcon} alt="#" />
         ) : (
           <img className="dl-f-icon" src={profileIcon} alt="#" />
         )}
       </Link>
-      <Link className="dl-f-l" to={`/auth/community/${user._id}`}>
+      <Link className={ commActive? `dlfl-r-active dlf-r dl-f-l`: `dl-f-l dlf-r`} to={`/auth/community/${user._id}`}>
         {commActive ? (
           <img className="dl-f-icon dl-f-active" src={communityActiveIcon} alt="#" />
         ) : (
